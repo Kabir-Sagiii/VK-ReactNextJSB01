6482e4c07ed4b87ef362478d155bfaf8ff884880
Kabir-Sagiii <sagithedeveloper@gmail.com>
2026-07-17 08:56:30 +0530

    added

---
+++ M03_ReactJS/S06_React-App/myapp/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Profile from '../profile/Profile.jsx'
+function Home() {
+  return (
+    <div>
+        <h1>Home Component</h1>
+        <Profile />
+    </div>
+  )
+}
+
+export default Home
